4cf037405a30f2a3938b0862fa9f2d3b113c414b
adda-sil <adrien@fernandes.bzh>
2022-01-18 16:05:56 +0100

    Make typedefs private

--- templates/vector.hpp
+++ templates/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: adda-sil <adda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/16 12:01:27 by adda-sil          #+#    #+#             */
-/*   Updated: 2022/01/18 15:54:36 by adda-sil         ###   ########.fr       */
+/*   Updated: 2022/01/18 16:05:48 by adda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,26 +21,32 @@ namespace ft
     template < class T, class Alloc = std::allocator<T> >
     class vector {
         public:
-            typedef T           value_type;
-            typedef Alloc       allocator_type;
-            typedef size_t      size_type;
-
             explicit vector (const allocator_type& alloc = allocator_type()) {
                 VDBG("Default Constructor");
                 (void) alloc;
             };
-            explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type());
+            explicit vector (
+                size_type n,
+                const value_type & val = value_type(),
+                const allocator_type & alloc = allocator_type()
+            );
             vector(const vector & x) {
                 VDBG("Copy Constructor");
                 *this = x;
             };
-            //template <class InputIterator>
-            //vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type());
+            template <class InputIterator>
+            vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type());
             ~vector(void) { VDBG("Destructor"); };
 
         private:
+            typedef T                   value_type;
+            typedef value_type &        reference;
+            typedef value_type *        pointer;
+            typedef Alloc               allocator_type;
+            typedef size_t              size_type;
 
         protected:
+            T * c;
     };
 }
 
