b7c9e18a0a3c7cef4b70fa215ae7acc690265ca9
adda-sil <adrien@fernandes.bzh>
2022-01-18 22:11:46 +0100

    Allocation / destruction memory

--- srcs/test_vector.cpp
+++ srcs/test_vector.cpp
@@ -6,15 +6,40 @@
 /*   By: adda-sil <adda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 15:09:31 by adda-sil          #+#    #+#             */
-/*   Updated: 2022/01/18 17:09:46 by adda-sil         ###   ########.fr       */
+/*   Updated: 2022/01/18 22:06:39 by adda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifdef STL //CREATE A REAL STL EXAMPLE
+	#include <map>
+	#include <stack>
+	#include <vector>
+	namespace ft = std;
+#else
+	#include "vector.hpp"
+#endif
+
 #include "tester.hpp"
+#include <vector>
 
 void test_vector_main(void)
 {
     std::cout << "TEST VECTORS" << std::endl;
     ft::vector<int> v;
-    std::cout << v.at(32) << std::endl;
+	v.push_back(666);
+	std::cout << "First " << v[0] << std::endl;
+
+
+    // ft::vector<int> v(10, 9);
+	// for (int i = 0;i < 10; i++)
+	// 	std::cout << "Iter " << i << " = " << v[i] << std::endl;
+	// v.reserve(30);
+	// for (int i = 0;i < 10; i++)
+	// 	std::cout << "Iter2 " << i << " = " << v[i] << std::endl;
+	// v.reserve(31);
+	// for (int i = 0;i < 10; i++)
+	// 	std::cout << "Iter3 " << i << " = " << v[i] << std::endl;
+
+    // std::cout << "At 2" << v[2] << std::endl;
+
 }
--- templates/vector.hpp
+++ templates/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: adda-sil <adda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/16 12:01:27 by adda-sil          #+#    #+#             */
-/*   Updated: 2022/01/18 17:20:33 by adda-sil         ###   ########.fr       */
+/*   Updated: 2022/01/18 22:09:42 by adda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ namespace ft
 
             // Member functions
             explicit vector (const allocator_type& alloc = allocator_type()) :
-                _size(0), _capacity(0)
+                _size(0), _capacity(0), _allocator(alloc)
             {
                 VDBG("Default Constructor");
                 (void) alloc;
@@ -51,32 +51,72 @@ namespace ft
                 size_type n,
                 const value_type & val = value_type(),
                 const allocator_type & alloc = allocator_type()
-            );
+            ) : _size(n), _capacity(n), _allocator(alloc) {
+                VDBG("Parametric Constructor: " << val << " - N > " << n);
+                VDBG("Size: " << _size);
+                VDBG("Capacity: " << _capacity);
+                this->_c = this->_allocator.allocate(n);
+                for (size_type i = 0; i < n; i++)
+                    this->_allocator.construct(this->_c + i, val);
+            }
             vector(const vector & x) {
                 VDBG("Copy Constructor");
                 *this = x;
             };
-            template <class InputIterator>
-            vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type());
-            ~vector(void) { VDBG("Destructor"); };
+            // template <class InputIterator>
+            // vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type());
+            ~vector(void) {
+                VDBG("Destructor");
+                for (size_type i = 0; i < this->_size; i++)
+                    this->_allocator.destroy(this->_c + i);
+                this->_allocator.deallocate(this->_c, this->_capacity);
+            };
 
             // Data access
             reference at(size_type position) {
+                VDBG("At position " << position);
                 if (!(position < this->size()))
                     throw std::out_of_range("out of range");
-                return this->c[position];
+                return this->_c[position];
             }
             const_reference at(size_type position) const {
+                VDBG("const At position " << position);
                 if (!(position < this->size()))
                     throw std::out_of_range("out of range");
-                return this->c[position];
+                return this->_c[position];
+            }
+
+            void push_back (const value_type& val) {
+                if (this->_size == this->_capacity) {
+                    VDBG("Max size reached");
+                    this->reserve(this->_capacity * 2 || 1);
+                }
+                this->at(this->_size++) = val;
+            }
+
+            reference operator[] (size_type position) {
+                return this->at(position);
+            }
+            const_reference operator[] (size_type position) const {
+                return this->at(position);
             }
 
             reference front() {
-                return this->c[0];
+                return *this->_c;
             };
             const_reference front() const {
-                return this->c[0];
+                return *this->_c;
+            }
+
+            reference back() {
+                return this->_c[this->_size];
+            }
+            const_reference back() const {
+                return this->_c[this->_size];
+            }
+
+            allocator_type get_allocator() const {
+                return this->_allocator;
             }
 
             // Size
@@ -84,7 +124,7 @@ namespace ft
                 return this->_size;
             }
             size_type max_size() const {
-                return this->_capacity;
+                return this->_allocator.max_size();
             }
             size_type capacity() const {
                 return this->_capacity;
@@ -96,17 +136,36 @@ namespace ft
              * If new_cap is greater than capacity(), all iterators, including the past-the-end iterator, and all references to the elements are invalidated. Otherwise, no iterators or references are invalidated.
              * @param new_cap 
              */
-            void reserve( size_type new_cap ) {
-                (void)new_cap;
+            void reserve(size_type new_cap) {
+                if (new_cap > this->max_size()) {
+                    throw std::length_error("Length error");
+                } else if (new_cap > this->_capacity) {
+                    VDBG("Reallocating " << new_cap);
+                    T *tmp = this->_allocator.allocate(new_cap);
+                    // Askip il faudra reconstruct + destroy pcq Maia l'a dit cf. Constructor
+                    for (size_type i = 0; i < this->_size; i++)
+                        tmp[i] = this->_c[i];
+                    if (this->_capacity)
+                        this->_allocator.deallocate(this->_c, this->_capacity);
+                    this->_c = tmp;
+                    this->_capacity = new_cap;
+                }
             }
 
-        protected:
-            T c[10];
-        
         private:
-            int         _size;
-            int         _capacity;
+            size_type                   _size;
+            size_type                   _capacity;
+            allocator_type              _allocator;
+            T *                         _c;
 
+            void    _upCapacity(size_type count) {
+                size_type target = this->_size + count;
+                if (target > this->_capacity) {
+                    if (target > this->max_size()) {
+                        throw std::exception();
+                    }
+                }
+            }
     };
 }
 
